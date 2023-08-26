89f3da1c37ee9ef6e6e90108b5b2e0bfd3068807
ignaciolynchb <ignaciolynchb@gmail.com>
2023-08-26 19:29:36 -0300

    Update My_New_Course.R

--- My_New_Course.R
+++ My_New_Course.R
@@ -6,15 +6,11 @@ library(swirlify)
 
 # The name of the new lesson is 'My First Lesson'
 # The name of the new course is 'My New Course'
-new_lesson("My First Lesson", "My New Course")
+new_lesson("Usando R como una calculadora", "My New Course")
 
 #Creating a New Lesson in an Existing Course
 #Make sure you’re working directory contains the course you want to add a lesson to. 
-new_lesson("My Second Lesson", "My New Course")
+#new_lesson("My Second Lesson", "My New Course")
 
 #You can explicitly specify the path to the lesson.yaml file.
-set_lesson("My_New_Course/My_First_Lesson/lesson.yaml")
-
-#Find Out What Lesson You’re Working On
-get_current_lesson()
-
+set_lesson("My_New_Course/Usando R como una calculadora/lesson.yaml")
