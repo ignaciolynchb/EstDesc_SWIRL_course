b546bac5700f38ae6d7542b1bcdbc56d348583e5
nachalca <nachalca@gmail.com>
2023-09-29 10:28:24 -0300

    instalar desde carpeta

--- Test.R
+++ Test.R
@@ -1,5 +1,10 @@
+
+# nacho: esto no tendria que ser necesario si usamos un proyecto, no?
 setwd("C:/Users/Nacho/Documents/GitHub/EstDesc_SWIRL_course")
+
+
 library(swirl)
-#uninstall_course("EstDesc SWIRL course")
-install_course_zip('My_New_Course.zip')
+#uninstall_course("My New Course")
+
+install_course_directory('My_New_Course')  # install_course_zip('My_New_Course.zip')
 swirl()
