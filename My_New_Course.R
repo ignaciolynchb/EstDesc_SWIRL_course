f3c402eb76790381cbad47538545a41eadc92915
ignaciolynchb <ignaciolynchb@gmail.com>
2023-10-04 11:19:11 -0300

    order lessons

--- My_New_Course.R
+++ My_New_Course.R
@@ -1,17 +1,17 @@
 # nacho estuvo aqui
-setwd("C:/Users/Nacho/Documents/GitHub/EstDesc_SWIRL_course")
+setwd("C:/Users/Nacho/Desktop/EstDesc_SWIRL_course")
 
 # Don't forget to load swirlfiy!
 #install.packages("swirlify")
 library(swirlify)
 
-# The name of the new lesson is 'My First Lesson'
+# The name of the new lesson is 'Introducción a R'
 # The name of the new course is 'My New Course'
-new_lesson("Usando R como una calculadora", "My New Course")
+new_lesson("Introducción a R", "My New Course")
 
 #Creating a New Lesson in an Existing Course
 #Make sure you’re working directory contains the course you want to add a lesson to. 
 #new_lesson("My Second Lesson", "My New Course")
 
 #You can explicitly specify the path to the lesson.yaml file.
-set_lesson("My_New_Course/Usando R como una calculadora/lesson.yaml")
+set_lesson("My_New_Course/Introducción a R/lesson.yaml")
--- Test.R
+++ Test.R
@@ -1,10 +1,5 @@
-
-# nacho: esto no tendria que ser necesario si usamos un proyecto, no?
-setwd("C:/Users/Nacho/Documents/GitHub/EstDesc_SWIRL_course")
-
-
 library(swirl)
-#uninstall_course("My New Course")
-
-install_course_directory('My_New_Course')  # install_course_zip('My_New_Course.zip')
+uninstall_course("My New Course")
+#install_course_zip('My_New_Course.zip')
+install_course_directory('My_New_Course')
 swirl()
